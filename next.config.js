529bd205b84536702c07e942750773c75b10bbc2
Roxi Zhauk <hiro@Hiros-MBP.local>
2023-04-10 22:07:37 +0900

    seventh commit

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   // productionBrowserSourceMaps: true,
+  output: "export",
   trailingSlash: true,
   basePath: process.env.GITHUB_ACTIONS && "/type-chart",
 };
