3678b17b942036d50df451057b52825db7785292
Roxi Zhauk <hiro@Hiros-MBP.local>
2023-04-11 08:48:21 +0900

    ninth commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/type-chart",
   experimental: {
     appDir: true,
   },
-  // productionBrowserSourceMaps: true,
-  output: "export",
   trailingSlash: true,
-  basePath: process.env.GITHUB_ACTIONS && "/type-chart",
 };
 
 module.exports = nextConfig;
