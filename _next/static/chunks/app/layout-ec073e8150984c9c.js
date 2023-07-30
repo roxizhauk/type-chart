3661fe58dc20f590f9e5d96e8ed272b1e24e7c57
roxizhauk <roxizhauk@users.noreply.github.com>
2023-07-30 10:34:29 +0000

    deploy: 65b21bf0d5871e57787eb40cb92f12896d3548f5

--- _next/static/chunks/app/layout-ec073e8150984c9c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4306:function(n,e,u){Promise.resolve().then(u.t.bind(u,8824,23))},8824:function(){}},function(n){n.O(0,[253,769,744],function(){return n(n.s=4306)}),_N_E=n.O()}]);
