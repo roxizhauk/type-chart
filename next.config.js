ed4706197b5e7b54e39edee68d4b5e6b0d1b9c45
Roxi Zhauk <roxizhauk@gmail.com>
2024-04-10 23:29:59 +0900

    updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: "export",
   basePath: process.env.GITHUB_ACTIONS && "/type-chart",
   trailingSlash: true,
 };
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
