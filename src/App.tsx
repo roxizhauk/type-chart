43b3433eaf3255d69e735db6e5359931d598da45
Roxi Zhauk <roxizhauk@gmail.com>
2024-04-26 14:06:49 +0900

    Add feTurbulence

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import { TypeChecker } from "@/components/type-checker";
 
 export default function App() {
   return (
-    <main>
-      <div className="bento">
-        <TypeChecker />
-      </div>
-    </main>
+    <>
+      <svg
+        className="pointer-events-none fixed isolate z-10 opacity-70 mix-blend-soft-light"
+        width="100%"
+        height="100%"
+      >
+        <filter id="noise">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.8"
+            numOctaves="4"
+            stitchTiles="stitch"
+          ></feTurbulence>
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noise)"></rect>
+      </svg>
+      <main className="">
+        <div className="bento">
+          <TypeChecker />
+        </div>
+      </main>
+    </>
   );
 }
